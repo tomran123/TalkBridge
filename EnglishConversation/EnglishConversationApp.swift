2093e5ede51f214eed0b887c5a5bcf5622c4880f
Rakan <charmerrk@gmail.com>
2023-08-07 09:08:43 +0800

    Initial Commit

---
+++ EnglishConversation/EnglishConversationApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnglishConversationApp.swift
+//  EnglishConversation
+//
+//  Created by Rakan on 2023/8/7.
+//
+
+import SwiftUI
+
+@main
+struct EnglishConversationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
