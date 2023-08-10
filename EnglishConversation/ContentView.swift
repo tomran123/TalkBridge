3d6f5de68dce23ed65a758626ab01745cfc10ec1
Rakan <charmerrk@gmail.com>
2023-08-10 15:45:30 +0800

    语音输出完成

--- EnglishConversation/ContentView.swift
+++ EnglishConversation/ContentView.swift
@@ -6,18 +6,154 @@
 //
 
 import SwiftUI
+import AVFoundation
+import Speech
+import ChatGPTSwift
+
+
+
 
 struct ContentView: View {
+    @State private var inputText = ""
+    @State private var messages: [Message] = []
+    @State private var isRecording = false
+    @State private var audioRecorder: AVAudioRecorder!
+    @State private var speechRecognizer = SFSpeechRecognizer()
+    @ObservedObject var recognizer = SpeechRecognizer()
+    @State private var currentRecordingMessageID: UUID?
+    @State private var textSend = ""
+    private var synthesizer = AVSpeechSynthesizer()
+    
+    let api = ChatGPTAPI(apiKey: "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")
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("AI英语口语练习应用")
+            Divider()
+                .frame(height: 1.0)
+            
+            
+            ScrollView {
+                Spacer()
+                ForEach(messages) { message in
+                    HStack(alignment: .top, content: {
+                        
+                        if message.isFromUser{
+                            ZStack {
+                                Circle()
+                                    .frame(width: 50, height: 50)
+                                    .foregroundColor(.orange)
+                                Text("ME")
+                                    .font(.title2)
+                                    .foregroundColor(Color.white)
+                                
+                            }
+                        }else{
+                            ZStack {
+                                Circle()
+                                    .frame(width: 50, height: 50)
+                                    .foregroundColor(.green)
+                                Text("AI")
+                                    .font(.title2)
+                                    .foregroundColor(Color.white)
+                            }
+                        }
+                        Text(message.text)
+                            .padding(.all)
+                            .background(.gray.opacity(0.2))
+                            .foregroundColor(.black)
+                            .cornerRadius(10)
+                            .frame(alignment: .leading)
+                        Spacer()
+                    })
+                    .padding(.leading, 10.0)
+                }
+            }
+            
+            HStack(spacing: 1.0) {
+                Button(action:{
+                    if recognizer.isRecording {
+                        recognizer.stopRecording()
+                        let processingMessage = Message(text: "processing...", isFromUser: false)
+                        messages.append(processingMessage)
+                        Task{
+                            do {
+                                let response = try await api.sendMessage(text: textSend)
+                                if let index = messages.firstIndex(where: { $0.text == "processing..." }) {
+                                    messages.remove(at: index)
+                                }
+                                let newMessage = Message(text: response, isFromUser: false)
+                                messages.append(newMessage)
+                                print(response)
+                                let utterance = AVSpeechUtterance(string: response)  // Use the API response
+                                utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+                                utterance.volume = 20.0  // Set the volume (optional)
+                                synthesizer.speak(utterance)
+                            } catch {
+                                print(error.localizedDescription)
+                            }
+                        }
+                        
+                        
+                    } else {
+                        let newMessage = Message(text: "recording...", isFromUser: true)
+                        messages.append(newMessage)
+                        currentRecordingMessageID = newMessage.id
+                        
+                        recognizer.onRecognizedTextUpdate = { updatedText in
+                            if let index = messages.firstIndex(where: { $0.id == currentRecordingMessageID }) {
+                                messages[index].text = updatedText
+                                textSend=messages[index].text
+                            }
+                        }
+                        
+                        
+                        recognizer.startRecording()
+                    }
+                    recognizer.isRecording.toggle()
+                }) {
+                    HStack {
+                        Image(systemName: "mic.fill")
+                            .foregroundColor(.white)
+                        Text(recognizer.isRecording ? "正在说话" : "开始录音")
+                            .foregroundColor(.white)
+                    }
+                    .padding()
+                    .background(recognizer.isRecording ? Color.green : Color.blue)
+                    .cornerRadius(10)
+                }
+                Spacer()
+                    .frame(width: 9.0)
+                Button(action:{
+                    messages=[]
+                    api.deleteHistoryList()
+                }) {
+                    HStack {
+                        Image(systemName: "trash.fill")
+                            .foregroundColor(.white)
+                        Text("删除对话")
+                            .foregroundColor(.white)
+                    }
+                    .padding()
+                    .background(Color.red)
+                    .cornerRadius(10)
+                }
+                
+            }
+            .padding()
+            .onAppear(perform: {
+                recognizer.requestAuthorization()
+            })
         }
-        .padding()
     }
 }
+
+struct Message: Identifiable {
+    let id = UUID()
+    var text: String
+    let isFromUser: Bool
+}
+
 
 #Preview {
     ContentView()
--- EnglishConversation/SpeechManager.swift
+++ EnglishConversation/SpeechManager.swift
@@ -5,7 +5,67 @@
 //  Created by Rakan on 2023/8/8.
 //
 
-import Foundation
 import AVFoundation
 import Speech
 
+class SpeechRecognizer: ObservableObject {
+    private var audioEngine = AVAudioEngine()
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionTask: SFSpeechRecognitionTask?
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "en-US"))
+    
+    @Published var isRecording = false
+    @Published var recognizedText: String = "点击按钮开始识别"
+    var onRecognizedTextUpdate: ((String) -> Void)?
+    
+    func requestAuthorization() {
+        SFSpeechRecognizer.requestAuthorization { status in
+            switch status {
+            case .authorized:
+                print("授权成功")
+            default:
+                print("未获得授权")
+            }
+        }
+    }
+
+    func startRecording() {
+        recognitionTask?.cancel()
+        recognitionTask = nil
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        
+        let inputNode = audioEngine.inputNode
+        guard let recognitionRequest = recognitionRequest else { return }
+        
+        audioEngine.prepare()
+        do {
+            try audioEngine.start()
+        } catch {
+            print("启动音频引擎出错: \(error)")
+        }
+        
+        recognitionTask = speechRecognizer?.recognitionTask(with: recognitionRequest) { [weak self] result, error in
+            DispatchQueue.main.async {
+                if let result = result {
+                    self?.recognizedText = result.bestTranscription.formattedString
+                    self?.onRecognizedTextUpdate?(result.bestTranscription.formattedString)
+                }
+
+                if error != nil {
+                    self?.stopRecording()
+                }
+            }
+        }
+        
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, _) in
+            recognitionRequest.append(buffer)
+        }
+    }
+
+    func stopRecording() {
+        audioEngine.stop()
+        recognitionRequest?.endAudio()
+        audioEngine.inputNode.removeTap(onBus: 0)
+    }
+}
